e4524e6e0f6bdd3ef6f3f9916a99557926df2b32
Ľuboš Dragan <102985621+DrLubos@users.noreply.github.com>
2024-05-22 14:08:05 +0200

    Implemented BinarySearchTree

--- TableManager.h
+++ TableManager.h
@@ -15,7 +15,8 @@ public:
 };
 
 TableManager::TableManager() {
-    table = new ds::adt::SortedSequenceTable<std::bitset<32>, ds::adt::ImplicitList<RoutingTableRow*>*>();
+    //table = new ds::adt::SortedSequenceTable<std::bitset<32>, ds::adt::ImplicitList<RoutingTableRow*>*>();
+    table = new ds::adt::BinarySearchTree<std::bitset<32>, ds::adt::ImplicitList<RoutingTableRow*>*>();
 }
 
 TableManager::~TableManager() {
